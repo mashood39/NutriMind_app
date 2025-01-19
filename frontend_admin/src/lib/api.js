c74d14258bb6e44e480ec94c24e9323d865468d7
MashoodCk <mashoodck77@gmail.com>
2025-01-19 14:33:49 +0530

    url

--- frontend_admin/src/lib/api.js
+++ frontend_admin/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // export const baseURL = "http://localhost:4000";
-export const baseURL = "https://nutrimindapp.vercel.app/"
+// export const baseURL = "https://nutrimindapp.vercel.app/"
+export const baseURL = "https://nutrimind-76defd8rm-mashoodcks-projects.vercel.app"
 
 
 const api = axios.create({
