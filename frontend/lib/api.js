3c28f9ff7377edfa4d94fb3c3d2c6c82d5838ee9
MashoodCk <mashoodck77@gmail.com>
2025-01-20 15:26:19 +0530

    url updated

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const baseURL = "http://10.32.1.107:4000";
+// export const baseURL = "http://10.32.1.107:4000";
 // export const baseURL = "http://192.168.158.9:4000";
-// export const baseURL = "https://nutrimindapp.vercel.app"
+export const baseURL = "https://nutrimindapp.vercel.app/"
 
 
 const api = axios.create({
